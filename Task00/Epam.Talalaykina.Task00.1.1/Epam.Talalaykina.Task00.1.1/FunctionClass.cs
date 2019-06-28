a17b935aff7ad29255ff94ad0e483f79843c768d
greenpenguin <penguin.is.green@gmail.com>
2019-06-28 21:53:32 +0400

    Add files via upload

---
+++ Task00/Epam.Talalaykina.Task00.1.1/Epam.Talalaykina.Task00.1.1/FunctionClass.cs
@@ -0,0 +1,24 @@
+﻿namespace Epam.Talalaykina.Task00._1._1
+{
+    public class FunctionClass
+    {
+        public string StringCreation(int n)
+        {
+            string resultString = "";
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n)
+                {
+                    resultString += i;
+                }
+                else
+                {
+                    resultString += i + ", ";
+                }
+                
+            }
+
+            return resultString;
+        }
+    }
+}
---
+++ Task00/Epam.Talalaykina.Task00.1.3/Epam.Talalaykina.Task00.1.3/SquareCreatingClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Epam.Talalaykina.Task00._1._3
+{
+    public class SquareCreatingClass
+    {
+        public List<string> SquareCreating(int n)
+        {
+            List<string> resultList = new List<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string resultString = "";
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i == n/2) && (j == n/2))
+                    {
+                        resultString += "  ";
+                    }
+                    else
+                    {
+                        resultString += "* ";
+                    }
+                }
+                resultString += "\n";
+                resultList.Add(resultString);
+            }
+
+            return resultList;
+        }
+    }
+}
